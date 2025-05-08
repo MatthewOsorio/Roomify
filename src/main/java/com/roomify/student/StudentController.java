f796a039c3ae295c5ecbe25241967f9a981bde11
MatthewOsorio <mattosoriox@gmail.com>
2025-05-07 19:17:44 -0700

    testing

--- src/main/java/com/roomify/student/StudentController.java
+++ src/main/java/com/roomify/student/StudentController.java
@@ -3,6 +3,7 @@ package com.roomify.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,10 @@ public class StudentController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
     }
+
+    @GetMapping(path = "test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test endpoint is working!");
+    }  
 }
 
